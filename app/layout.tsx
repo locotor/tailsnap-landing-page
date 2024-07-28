ad1c85170582be98b275d5eac4aca1015c7b1ca9
locotor <locotor@outlook.com>
2024-07-28 14:00:46 +0800

    feat: add a blog page

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,9 @@ export default async function RootLayout({
 }) {
   return (
     <html lang={lang || defaultLocale} suppressHydrationWarning>
-      <head />
+      <head>
+        <meta name="baidu-site-verification" content="codeva-jln3BDyHNA" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
